d8519ac0b3b54fdc39c8d262963a41a0ec006292
Gianmarco Incocciati <minusgian@gmail.com>
2023-05-06 16:24:11 +0200

    add $unique_str

--- partials/functions.php
+++ partials/functions.php
@@ -1,10 +1,11 @@
 <?php
 function getRandomPassword($length) {
     $my_string = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!?@#$%"£&/()=^[]{}°-_:.;,<>';
+    $unique_str = implode(',', array_unique(explode(',', $my_string)));
     $random_string = '';
     while ($length > 0) {
-        $random_num = rand(0, strlen($my_string) - 1);
-        $random_string .= $my_string[$random_num];
+        $random_num = rand(0, strlen($unique_str) - 1);
+        $random_string .= $unique_str[$random_num];
         $length--;
     }
     return $random_string;
